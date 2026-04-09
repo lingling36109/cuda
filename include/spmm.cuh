67afa1d51fbce47f5010591639973c168061964e
ajk345 <ajk345@cornell.edu>
2026-04-09 12:42:07 -0400

    some stuff

--- include/spmm.cuh
+++ include/spmm.cuh
@@ -10,23 +10,30 @@ __global__ void SpMM(const size_t m, const size_t n, const size_t k,
                      float * d_A_vals, uint32_t * d_A_colinds, uint32_t * d_A_rowptrs,
                      __constant__ float * d_X, float * d_Y)
 {
-    /**** IMPLEMENT THIS KERNEL ****/
-	//TODO
+    size_t row = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row >= m) return;
+
+    uint32_t row_start = d_A_rowptrs[row];
+    uint32_t row_end = d_A_rowptrs[row + 1];
+
+    for (size_t j = 0; j < k; j++) {
+        float sum = 0.0f;
+        for (uint32_t idx = row_start; idx < row_end; idx++) {
+            sum += d_A_vals[idx] * d_X[d_A_colinds[idx] * k + j];
+        }
+        d_Y[row * k + j] = sum;
+    }
 }
 
 
 void SpMM_wrapper(csr_t& A, float * d_X, float * d_Y, const size_t k)
 {
-    //**** CHANGE THESE VALUES ****//
-    uint32_t threads_per_block = 1;
-    uint32_t blocks = 1; 
-
-    //**** OPTIONAL: Preprocessing/load balancing step ****//
-    // TODO
+    uint32_t threads_per_block = 256;
+    uint32_t blocks = (A.get_rows() + threads_per_block - 1) / threads_per_block;
 
     // Call the kernel
     SpMM<<<blocks, threads_per_block>>>(A.get_rows(), A.get_cols(), k,
-                                        A.get_vals(), A.get_colinds(), A.get_rowptrs(), 
+                                        A.get_vals(), A.get_colinds(), A.get_rowptrs(),
                                         d_X, d_Y);
 
     // Sync w/ the host
